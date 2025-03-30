1c78ede42a50633171f08503b130fcf130333fe6
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-30 21:24:40 +0100

    updated main so it calls the login html file which has all the buttons i needed and now we have the LogIn.css to make it look a lot more presentable

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #constructer for the flask app and it sets up the application
 app = Flask(__name__)
 
 # this is to create the url that the function underneath will respond to
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    #this is to link to the LogIn.html file
+    return render_template("LogIn.html")
 
 # makes the flask app run when this file is executed directly not when imported
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
