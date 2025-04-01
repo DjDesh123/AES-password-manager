3e76660dd20a0bf22a0429b4f3fa3713aebe3e9b
Deshawn Johal <deshawnjohal@gmail.com>
2025-04-01 20:54:36 +0100

    added better css to the log in page and now need to implement the json database and then then code the functionality to the button so it actually checks the infomation through the database

---
+++ static/js/Login.js
@@ -0,0 +1,4 @@
+// this function will be taking the user credentials and then checking if they are a part of the database that needs to be implemented via json file
+function CheckCredentials(){
+
+}
