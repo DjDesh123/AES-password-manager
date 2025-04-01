0f9e48efb38c232e6824c67ae72f65f0c58760a2
Deshawn Johal <deshawnjohal@gmail.com>
2025-04-02 00:31:25 +0100

    moved and documented the initalise of the java file that's being utilised for the login then will use this to link back to the flask database that still needs to be created

--- static/js/Login.js
+++ static/js/Login.js
@@ -1,4 +1,9 @@
 // this function will be taking the user credentials and then checking if they are a part of the database that needs to be implemented via json file
-function CheckCredentials(){
+function CheckCredentials(username,password){
+    console.log("external js working ");
+    //gets the username and password
+    var userInput1 = document.getElementById(username).value;
+    var userInput2 = document.getElementById(password).value;
 
+    // need to make the post thing to send it back to app so then i can check the credentials of the log in through anotehr files database like planned
 }
