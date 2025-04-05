b2e3c0b19c21a868bb482e548f58b7a0f6d16c4a
Deshawn Johal <deshawnjohal@gmail.com>
2025-04-05 13:40:34 +0100

    added a route to the sign up page

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect,url_for
 
 #constructer for the flask app and it sets up the application
 app = Flask(__name__)
 
 # this is to create the url that the function underneath will respond to
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     #this is to link to the LogIn.html file
     return render_template("LogIn.html")
+
+
+#this will be handling the signup page which we need to create
+@app.route('/SignUp', methods=['GET', 'POST'])
+def signUp():
+    return render_template("SignUp.html")
+
 
 # makes the flask app run when this file is executed directly not when imported
 if __name__ == '__main__':
