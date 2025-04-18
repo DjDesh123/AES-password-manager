b6579219bb4d323a9809ce8f59f457e119c8cdec
Deshawn Johal <deshawnjohal@gmail.com>
2025-04-19 00:17:38 +0100

    fixed the issue with calling the external js script

--- static/js/Login.js
+++ static/js/Login.js
@@ -1,9 +1,25 @@
-// this function will be taking the user credentials and then checking if they are a part of the database that needs to be implemented via json file
-function CheckCredentials(username,password){
-    console.log("external js working ");
-    //gets the username and password
-    var userInput1 = document.getElementById(username).value;
-    var userInput2 = document.getElementById(password).value;
+function CheckCredentials(e){
+    console.log("working");
+    // to cancel the event if the user misses an entry box
+    e.preventDefault();
 
-    // need to make the post thing to send it back to app so then i can check the credentials of the log in through anotehr files database like planned
+    //gets the username and password
+    var userInput1 = document.getElementById("username").value;
+    var userInput2 = document.getElementById("password").value;
+
+    if(!userInput1 || !userInput2){
+        alert("both fields are required!");
+    }
+
+
 }
+
+
+/* thought process
+basically in the program you need to check the credentials first and foremost and that's the first button thing we need to do and reduce repeated code
+then we need to send the credentials over without an issue so we would need to send this info to main which then would reroute to argon2
+then aes encryption then save boom, but first we need it to go back to main so the routeing is the first thing we need to handle
+when this works we just need to rinse and repeat and make the ui look nice
+
+i saw an example of fetch being used in someone code and i want to try to implement it myself into my own code.
+ */
